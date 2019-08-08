62f8e57551f11cd3bfc8b07e25c6e1fccff96dc4
Willy Patiño <30963719+Hackalmighty@users.noreply.github.com>
2019-08-07 20:20:51 -0500

    modelo usuario
    agregando getters and setters al modelo usuario

--- app/src/main/java/jcv/com/mascotas/modelo/Usuario.java
+++ app/src/main/java/jcv/com/mascotas/modelo/Usuario.java
@@ -2,12 +2,16 @@ package jcv.com.mascotas.modelo;
 
 public class Usuario {
     public String token;
+    public String username;
+    public String password;
 
     public Usuario() {
     }
 
-    public Usuario(String token) {
+    public Usuario(String token, String username, String password) {
         this.token = token;
+        this.username = username;
+        this.password = password;
     }
 
     public String getToken() {
@@ -16,5 +20,21 @@ public class Usuario {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
