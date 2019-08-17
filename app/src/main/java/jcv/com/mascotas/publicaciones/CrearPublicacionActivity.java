1a56fce87a1f7476189e3ec4cde79d99712d6be3
Vyenavi <37272161+Vyenavi@users.noreply.github.com>
2019-08-17 14:00:32 -0500

    Update CrearPublicacionActivity.java

--- app/src/main/java/jcv/com/mascotas/publicaciones/CrearPublicacionActivity.java
+++ app/src/main/java/jcv/com/mascotas/publicaciones/CrearPublicacionActivity.java
@@ -11,5 +11,6 @@ public class CrearPublicacionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_publicacion);
+        String locasl;
     }
 }
