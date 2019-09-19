33c9050bf5abb6dceff8bacda525f627b7f57786
Vyenavi <37272161+Vyenavi@users.noreply.github.com>
2019-09-19 17:32:56 -0500

    detalle publicacion

--- app/src/main/java/jcv/com/mascotas/publicaciones/DetallePublicacionActivity.java
+++ app/src/main/java/jcv/com/mascotas/publicaciones/DetallePublicacionActivity.java
@@ -1,78 +1,99 @@
 package jcv.com.mascotas.publicaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import jcv.com.mascotas.R;
+import jcv.com.mascotas.mascota.DetalleMascotaActivity;
+import jcv.com.mascotas.mascota.MascotaEditarActivity;
+import jcv.com.mascotas.utils.BottomNavigationViewHelper;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DetallePublicacionActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int SWIPE_MIN_DISTANCE = 120;
-    private static final int SWIPE_MAX_OFF_PATH = 250;
-    private static final int SWIPE_THRESHOLD_VELOCITY = 30;
-    private GestureDetector mGestureDetector;
-    private View.OnTouchListener mGestureListener;
-    private ImageView mImageView;
-    private Integer[] mImagesList = { R.drawable.ic_action_call, R.drawable.ic_action_edit,
-            R.drawable.ic_action_edit, R.drawable.ic_action_regresar};
-    private int mPhoto;
-    private RelativeLayout mView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DetallePublicacionActivity extends AppCompatActivity {
+    private static final int ACTIVITY_NUM = 3;
+    private Context mcontext = DetallePublicacionActivity.this;
+    private BottomNavigationView bottomNavigationView;
+    private BottomNavigationViewHelper bottomNavigationViewHelper;
+    private ImageView regresar;
+    private TextView nombreCabecera;
+
+    private androidx.appcompat.widget.Toolbar toolbarPerfilMascota;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_publicacion);
-        mPhoto = 0;
-        mView = (RelativeLayout) findViewById(R.id.vista_fotos);
-        mImageView = (ImageView) findViewById(R.id.image);
+        findElemente();
+        //setUpBottomNavigationview();
+        //setUpToolBar();
+        eventos();
 
-        // initialize with the first image
-        mImageView.setImageResource(mImagesList[mPhoto]);
-
-        // Gesture detection
-        mGestureDetector = new GestureDetector(new MyGestureDetector());
-        mGestureListener = new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                return mGestureDetector.onTouchEvent(event);
-            }
-        };
-        // prevent the view to be touched
-        mView.setOnClickListener(this);
-        mView.setOnTouchListener(mGestureListener);
     }
-    public class MyGestureDetector extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                               float velocityY) {
-            try {
-                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-                    return false;
-                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                    mPhoto = (mPhoto + 1) % mImagesList.length;
-                    mImageView.setImageResource(mImagesList[mPhoto]);
-                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                    mPhoto = (mPhoto - 1) % mImagesList.length;
-                    if (mPhoto < 0) {
-                        mPhoto = 0;
-                    }
-                    mImageView.setImageResource(mImagesList[mPhoto]);
+    private void findElemente() {
+        regresar =(ImageView) findViewById(R.id.regresarPerfilEditar);
+        nombreCabecera = (TextView) findViewById(R.id.perfilUsuarioNombreEditar);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        toolbarPerfilMascota = findViewById(R.id.toolbarperfilEditar);
+    }
+
+    private void setUpToolBar(){
+        setSupportActionBar(toolbarPerfilMascota);
+        toolbarPerfilMascota.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_perfil_mascota_edit:
+                        Intent intentEditPerfilMascota = new Intent(getApplicationContext(), MascotaEditarActivity.class);
+                        startActivity(intentEditPerfilMascota);
+                        break;
+                    case R.id.menu_perfil_mascota_eliminar:
+                        Toast.makeText(mcontext, "Se elimina por id", Toast.LENGTH_SHORT).show();
+                        break;
+
                 }
-            } catch (Exception e) {
-                Log.e("SwypeImagesActivity", "error on gesture detector");
+                return false;
             }
-            return false;
-        }
+        });
+    }
+    private void setUpBottomNavigationview() {
+        //bottomNavigationViewHelper.setUpBottomNavigationView(bottomNavigationView);
+        bottomNavigationViewHelper.enableBottomNavigationView(bottomNavigationView, mcontext);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
     }
 
     @Override
-    public void onClick(View view) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_perfil_mascota,menu);
+        return true;
+    }
+    private void eventos() {
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regresarHome = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(regresarHome);
+            }
+        });
+
+        nombreCabecera.setText("Detalle Publicación");
 
     }
+
 }
