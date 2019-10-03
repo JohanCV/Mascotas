3937963eed45b2417987a498df0b97e96050cb0e
Gohan <nahoj1992@gmail.com>
2019-10-03 18:05:14 -0500

    cambion
    cambion

--- app/src/main/java/jcv/com/mascotas/Encontrar/EncontrarActivity.java
+++ app/src/main/java/jcv/com/mascotas/Encontrar/EncontrarActivity.java
@@ -7,8 +7,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 import jcv.com.mascotas.R;
 import jcv.com.mascotas.utils.BottomNavigationViewHelper;
@@ -20,11 +29,41 @@ public class EncontrarActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private BottomNavigationViewHelper bottomNavigationViewHelper;
 
+    private ImageView regresar;
+    private TextView nombreCabecera;
+    private Button btnSave;
+    private EditText nombre;
+    private EditText apellido;
+    RadioButton rb;
+    private EditText Email;
+    private EditText Telefono;
+    Spinner departamento;
+    Spinner provincia;
+    Spinner distrito;
+    private EditText Contrasena;
+    private EditText RepeteContrasena;
+    String sexo = "M";
+    List<String[]> ubigeo;
+    List<List<String[]>> ubigeoDistrito;
+    int provinciaSeleccionada = 0;
+    private ImageButton SubirFoto;
+    private ImageView Fotoperfil;
+    private static final int TAKE_PICTURE = 1;
+    private static final int CAMERA_REQUEST1 = 1887;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int ACTIVITY_SELECT_IMAGE = 1020,
+            ACTIVITY_SELECT_FROM_CAMERA = 1040, ACTIVITY_SHARE = 1030;
+
+
+    String[] datosdesexo = { "Hembra", "Macho", "Otro"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_encontrar);
 
         Log.d(TAG, " On create: Starting");
 
